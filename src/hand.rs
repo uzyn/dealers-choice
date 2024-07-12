dd7ecfd2e5af9e361ca31229a9027a2b82c58fea
U-Zyn Chua <chua@uzyn.com>
2024-07-12 16:41:58 +0800

    Print hand

--- src/hand.rs
+++ src/hand.rs
@@ -10,7 +10,7 @@ impl Hand {
         Hand { cards: Vec::new() }
     }
 
-    //TODO: Sort and display of Hand
+    //TODO: Sort
 }
 
 impl Default for Hand {
@@ -19,13 +19,32 @@ impl Default for Hand {
     }
 }
 
+impl std::fmt::Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for card in &self.cards {
+            write!(f, "{}", card)?;
+        }
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::card::{Card, Rank, Suit};
 
     #[test]
     fn get_new_hand() {
         let hand = Hand::new();
         assert_eq!(hand.cards.len(), 0);
+    }
+
+    #[test]
+    fn display_hand_in_string() {
+        let mut hand = Hand::new();
+        hand.cards.push(Card::new(Suit::Club, Rank::King));
+        hand.cards.push(Card::new(Suit::Diamond, Rank::Deuce));
+        hand.cards.push(Card::new(Suit::Heart, Rank::Jack));
+        assert_eq!(hand.to_string(), "Kc2dJh");
     }
 }
